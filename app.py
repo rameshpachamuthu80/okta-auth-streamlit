3baebdd132f5fce0d0d20cbbaee47056bf9512c1
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-01-26 20:27:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-st.write(st.experimental_user)
 
-# if not st.experimental_user.is_logged_in:
-#   if st.button("Log in"):
-#     st.login()
-# else:
-#   if st.button("Log out"):
-#     st.logout()
+if not st.experimental_user.is_logged_in:
+  if st.button("Log in"):
+    st.login()
+else:
+  if st.button("Log out"):
+    st.logout()
   
-#   st.write(f"Hello, {st.experimental_user.name}!")
+  st.write(f"Hello, {st.experimental_user.name}!")
