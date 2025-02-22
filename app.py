4df0ab7f71d7ddf3333d8fed4ee3a104dbf73758
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-02-22 18:01:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
 
+st.title("Okta Auth Demo")
+
 if not st.experimental_user.is_logged_in:
-  if st.button("Log in"):
     st.login()
 else:
-  if st.button("Log out"):
-    st.logout()
-  
-  st.write(f"Hello, {st.experimental_user.name}!")
+  st.write(st.experimental_user)
