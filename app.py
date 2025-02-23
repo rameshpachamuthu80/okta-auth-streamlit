d1a5ce5519525b7632d98dd0c34961807cbff71a
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-02-23 12:06:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,4 @@ if not st.experimental_user.is_logged_in:
 st.button("Log out", on_click=st.logout)
 st.write(st.experimental_user)
 
-id_token = st.experimental_user.id_token  # Get the raw ID token
-decoded_token = jwt.decode(id_token, options={"verify_signature": False})  
-st.write("Decoded Token:", decoded_token) 
-
 
