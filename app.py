8dce76d2e9a878787e7d2d380780d5be3401682b
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-02-23 12:00:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import jwt
 
 if not st.experimental_user.is_logged_in:
     st.button("Log in with Okta", on_click=st.login)
@@ -6,3 +7,9 @@ if not st.experimental_user.is_logged_in:
 
 st.button("Log out", on_click=st.logout)
 st.write(st.experimental_user)
+
+id_token = st.experimental_user.id_token  # Get the raw ID token
+decoded_token = jwt.decode(id_token, options={"verify_signature": False})  
+st.write("Decoded Token:", decoded_token) 
+
+
