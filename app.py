aa35c48c681550b2f7702d48765d010860a34070
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-02-23 12:40:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import jwt
 
 if not st.experimental_user.is_logged_in:
     st.button("Log in with Okta", on_click=st.login)
     st.stop()
 
 st.button("Log out", on_click=st.logout)
-st.write(st.experimental_user)
+#st.write(st.experimental_user)
+st.write(f"Welcome , {st.experimental_user.name}!")
 
 
