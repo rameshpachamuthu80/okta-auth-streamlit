b89f54ff7ca6d92771df7808f9538baeb2a47aed
Oleksandr Arsentiev <108185255+arsentievalex@users.noreply.github.com>
2025-02-23 18:32:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ if not st.experimental_user.is_logged_in:
 st.button("Log out", on_click=st.logout)
 #st.write(st.experimental_user)
 st.write(f"Welcome {st.experimental_user.name}!")
+st.write(f"Role: {st.experimental_user.job_title}!")
 
 
