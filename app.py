1b422594768bb9b4870ba77117299237abfd6eab
rameshpachamuthu80 <rameshpachamuthu@gmail.com>
2025-04-08 14:54:11 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="Okta Auth Demo", page_icon='🔐', initial_sidebar_state="auto", menu_items=None)
+
+if not st.experimental_user.is_logged_in:
+    st.login()
+    st.stop()
